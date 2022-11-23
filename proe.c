e74f7e96758366f1b5e04a510cf11400be574a11
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-11-23 11:45:10 +0900

    Add proe.c

--- proe.c
+++ proe.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MEM_SIZE 10
+#define MEM_SIZE 100
 #define READ_FILE "in.txt"
 #define WRITE_FILE "out.txt"
 
@@ -16,15 +16,22 @@ int main()
     /* 入力ファイル保存用の、配列の容量確保 */
     txt = (char*)malloc( sizeof(char) * MEM_SIZE );
 
-    /* 配列の容量確保失敗 */
+    /* 配列のメモリ確保失敗 */
     if(txt == NULL)
     {
-        printf("failed to get memory size of txt...\n");
+        perror("配列のメモリ確保に失敗しました。\n");
         return 1;
     }
 
     /* 入力ファイルを開く */
     read = fopen(READ_FILE, "r");
+
+    /* 入力ファイルを開く失敗 */
+    if(read == NULL)
+    {
+        perror("入力ファイルへのアクセスに失敗しました。\n");
+        return 1;
+    }
 
     /* 配列へテキストを格納 */
     for(i = 0; i < MEM_SIZE; i++)
@@ -39,25 +46,26 @@ int main()
         }
     }
 
+    /* 入力ファイルを閉じる */
+    fclose(read);
 
+    /* 出力ファイルを開く */
+    write = fopen(WRITE_FILE, "w");
 
-    while( (txt = fgetc(read)) != EOF )
+    /* 出力ファイルを開く失敗 */
+    if(write == NULL)
     {
-        fprintf(read, "%c", txt);
-        printf("%c", c);
+        perror("出力ファイルへのアクセスに失敗しました。\n");
+        return 1;
     }
-                
 
-    /* 配列へ、テキストを格納 */
+    /* 出力ファイルへ配列を入力 */
     for(i = 0; i < MEM_SIZE; i++)
     {
-    
-        txt[i] = c;
-        printf("txt[%d] = %c\n", i, txt[i]);
+        c = txt[i];
+        fprintf(write, "%c", c);
     }
 
-    /* 配列を出力ファイルに出力 */
-    write = fopen(WRITE_FILE, "w");
-    fprintf(write, "%s\n", txt);
+    /* 出力を閉じる */
     fclose(write);
 }
