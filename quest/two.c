a370794681226da2e047c0adcdb6e358aed24e1c
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-11-30 02:34:15 +0900

    Add index

--- quest/two.c
+++ quest/two.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <time.h>
 
-#define MEM_SIZE    1000         // 読み込み可能最大文字数
+#define MAX_CHAR    1000         // 読み込み可能最大文字数
 #define READ_FILE   "in.txt"     // 入力ファイル
 #define WRITE_FILE  "out.txt"    // 出力ファイル
-#define RANDOM_NUM  1000          // ランダムに生成する文字数
-
-unsigned int Get_random (unsigned int get_min, unsigned int get_max);
-
-unsigned int Get_random (unsigned int get_min, unsigned int get_max)
-{
-    return get_min + (unsigned int)( rand() * (get_max - get_min + 1.0) / (RAND_MAX + 1.0) );
-}
-
-
 
 int main ()
 {
@@ -29,9 +18,8 @@ int main ()
     FILE* write = NULL;
 
 
-
-    /* 入力ファイル保存用の、配列のメモリ確保 */
-    txt = (char*)malloc( sizeof(char) * MEM_SIZE );
+    /* 配列のメモリ確保（入力保存用）*/
+    txt = (char*)malloc( sizeof(char) * MAX_CHAR );
 
     /* 配列のメモリ確保失敗 */
     if (txt == NULL)
@@ -39,7 +27,6 @@ int main ()
         perror("\n配列のメモリ確保に失敗しました。\n");
         return 1;
     }
-
 
 
     /* 入力ファイルを開く ------------------------------*/
@@ -52,7 +39,7 @@ int main ()
         return 1;
     }
 
-    /* 配列へ入力ファイルを格納 */
+    /* 配列へ文字を格納 */
     while (1)
     {
         in_buf = fgetc(read);
@@ -68,19 +55,12 @@ int main ()
     fclose(read);
 
 
-
-    /* 頻度を求める */
+    /* 文字の出現頻度を求める */
     for (j = 0; j <= in_max; j++)
     {
         for (i = 0; i <= 25; i++) if ( txt[j] == (65 + i)  ||  txt[j] == (97 + i) ) fleq[i] ++;
         if (txt[j] == 32) fleq[26] ++;
     }
-
-
-
-    /* 乱数 */
-    srand( (unsigned int)time(NULL) );
-
 
 
     /* 出力ファイルを開く ------------------------------*/
@@ -93,14 +73,9 @@ int main ()
         return 1;
     }
 
-    /* 出力ファイルに入力 */
-    for (i = 0; i <= RANDOM_NUM; i++)
-    {
-        in_buf = txt[ Get_random(0, in_max) ];
-        fprintf(write, "%c", in_buf);
-
-        printf("%u = random, %u = in_max\n", Get_random(0, in_max), in_max);
-    }
+    /* 文字の出現頻度を書き込む */
+    for (i = 0; i <= 25; i++) fprintf (write, "%u %c found\n", fleq[i], (97 + i) );
+    fprintf (write, "%u '%c' found\n", fleq[26], 32);
 
     /* 出力を閉じる ------------------------------------*/
     fclose(write);
--- quest/two3.c
+++ quest/two3.c
@@ -1,45 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <time.h>
 
-#define MEM_SIZE    1000         // 読み込み可能最大文字数
+#define CHAR_QUA    10000        // 最大文字数
 #define READ_FILE   "in.txt"     // 入力ファイル
 #define WRITE_FILE  "out.txt"    // 出力ファイル
-#define RANDOM_NUM  1000          // ランダムに生成する文字数
-
-unsigned int Get_random (unsigned int get_min, unsigned int get_max);
-
-unsigned int Get_random (unsigned int get_min, unsigned int get_max)
-{
-    return get_min + (unsigned int)( rand() * (get_max - get_min + 1.0) / (RAND_MAX + 1.0) );
-}
-
-
 
 int main ()
 {
     char in_buf = 0;
-    char* txt = {0};
+    char txt[CHAR_QUA] = {0};
     unsigned int in_max = 0;
     unsigned int i = 0;
     unsigned int j = 0;
     unsigned int fleq[27] = {0};
     FILE* read = NULL;
     FILE* write = NULL;
-
-
-
-    /* 入力ファイル保存用の、配列のメモリ確保 */
-    txt = (char*)malloc( sizeof(char) * MEM_SIZE );
-
-    /* 配列のメモリ確保失敗 */
-    if (txt == NULL)
-    {
-        perror("\n配列のメモリ確保に失敗しました。\n");
-        return 1;
-    }
-
 
 
     /* 入力ファイルを開く ------------------------------*/
@@ -52,7 +28,7 @@ int main ()
         return 1;
     }
 
-    /* 配列へ入力ファイルを格納 */
+    /* 配列へ文字を格納 */
     while (1)
     {
         in_buf = fgetc(read);
@@ -68,19 +44,12 @@ int main ()
     fclose(read);
 
 
-
-    /* 頻度を求める */
+    /* 文字の出現頻度を求める */
     for (j = 0; j <= in_max; j++)
     {
         for (i = 0; i <= 25; i++) if ( txt[j] == (65 + i)  ||  txt[j] == (97 + i) ) fleq[i] ++;
         if (txt[j] == 32) fleq[26] ++;
     }
-
-
-
-    /* 乱数 */
-    srand( (unsigned int)time(NULL) );
-
 
 
     /* 出力ファイルを開く ------------------------------*/
@@ -93,14 +62,9 @@ int main ()
         return 1;
     }
 
-    /* 出力ファイルに入力 */
-    for (i = 0; i <= RANDOM_NUM; i++)
-    {
-        in_buf = txt[ Get_random(0, in_max) ];
-        fprintf(write, "%c", in_buf);
-
-        printf("%u = random, %u = in_max\n", Get_random(0, in_max), in_max);
-    }
+    /* 文字の出現頻度を書き込む */
+    for (i = 0; i <= 25; i++) fprintf (write, "%u %c found\n", fleq[i], (97 + i) );
+    fprintf (write, "%u '%c' found\n", fleq[26], 32);
 
     /* 出力を閉じる ------------------------------------*/
     fclose(write);
