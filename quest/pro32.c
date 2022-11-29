6aa7d01ddc16cd4a2636c26dc2b319d43e810ac2
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-11-30 06:32:42 +0900

    32

--- quest/pro32.c
+++ quest/pro32.c
@@ -11,16 +11,13 @@ unsigned char Get_freq (unsigned char elem);
 int main ()
 {
     char one_buf = 0;
-    char two_buf = 0;
     char txt[CHAR_QUA] = {};
     unsigned char x_buf = 0;
     unsigned char y_buf = 0;
-    unsigned char z_buf = 0;
     unsigned int in_max = 0;
     unsigned int i = 0;
     unsigned char j = 0;
-    unsigned char k = 0;
-    unsigned int freq[27][27][27] = {};
+    unsigned int freq[27][27] = {};
     FILE* read = NULL;
     FILE* write = NULL;
 
@@ -54,17 +51,15 @@ int main ()
     /* 文字の出現頻度を求める */
     for (i = 0; i <= in_max; i++)
     {
-        x_buf = Get_freq ( two_buf );
-        y_buf = Get_freq ( one_buf );
-        z_buf = Get_freq ( txt[i] );
+        x_buf = Get_freq ( one_buf );
+        y_buf = Get_freq ( txt[i] );
 
-        if (one_buf != 0 && two_buf != 0)
+        if (one_buf != 0)
         {
-            freq[ x_buf ][ y_buf ][ z_buf ] ++ ;
+            freq[ x_buf ][ y_buf ] ++ ;
         }
 
         one_buf = txt[i];
-        two_buf = one_buf;
     }
 
 
@@ -83,39 +78,19 @@ int main ()
     {
         for (j = 0; j <= 25; j++)
         {
-            for (k = 0; k <= 25; k++)
-            {
-                fprintf(write, "[%c%c%c] =", (97 + i), (97 + j), (97 + k));
-                fprintf(write, "%u\n", freq[i][j][k]);
-            }
-            fprintf(write, "[%c%c%c] =", (97 + i), (97 + j), 32);
-            fprintf(write, "%u\n", freq[i][j][26]);
+            fprintf(write, "[%c%c] =", (97 + i), (97 + j));
+            fprintf(write, "%u\n", freq[i][j]);
         }
-        for (k = 0; k <= 25; k++)
-        {
-            fprintf(write, "[%c%c%c] =", (97 + i), 32, (97 + k));
-            fprintf(write, "%u\n", freq[i][26][k]);
-        }
-        fprintf(write, "[%c%c%c] =", (97 + i), 32, 32);
-        fprintf(write, "%u\n", freq[i][26][26]);
+        fprintf(write, "[%c%c] =", (97 + i), 32);
+        fprintf(write, "%u\n", freq[i][26]);
     }
     for (j = 0; j <= 25; j++)
     {
-        for (k = 0; k <= 25; k++)
-        {
-            fprintf(write, "[%c%c%c] =", 32, (97 + j), (97 + k));
-            fprintf(write, "%u\n", freq[26][j][k]);
-        }
-        fprintf(write, "[%c%c%c] =", 32, (97 + j), 32);
-        fprintf(write, "%u\n", freq[26][j][26]);
+        fprintf(write, "[%c%c] =", 32, (97 + j));
+        fprintf(write, "%u\n", freq[26][j]);
     }
-    for (k = 0; k <= 25; k++)
-    {
-        fprintf(write, "[%c%c%c] =", 32, 32, (97 + k));
-        fprintf(write, "%u\n", freq[26][26][k]);
-    }
-    fprintf(write, "[%c%c%c] =", 32, 32, 32);
-    fprintf(write, "%u\n", freq[26][26][26]);
+    fprintf(write, "[%c%c] =", 32, 32);
+    fprintf(write, "%u\n", freq[26][26]);
 
     /* 出力を閉じる ------------------------------------*/
     fclose(write);
